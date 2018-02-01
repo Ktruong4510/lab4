86d3b219eb4b377668313dbe7a5a3a1edf83ec20
Kenneth Truong <ket032@ucsd.edu>
2018-02-01 00:14:55 -0800

    lab4

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var handlebars = require('express3-handlebars')
-
 var index = require('./routes/index');
 var project = require('./routes/project');
 var hello = require('./routes/hello');
@@ -39,7 +38,6 @@ if ('development' == app.get('env')) {
 // Add routes here
 app.get('/', index.view);
 app.get('/project/:name', project.viewProject);
-app.get('/hello/:userName', hello.view);
 // Example route
 // app.get('/users', user.list);
 
